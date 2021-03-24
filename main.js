f5a5e0085d0333c12447c913e80852b432c8a9ce
pjy1368 <basejin0810@naver.com>
2021-03-24 22:08:28 +0900

    style(main): 학습용 주석 추가

--- main.js
+++ main.js
@@ -9,20 +9,20 @@ todoButton.addEventListener("click", addTodo)
 todoList.addEventListener("click", deleteCheck)
 filterOption.addEventListener("click", filterTodo)
 
-
 // functions
 function addTodo(event) {
     event.preventDefault();
 
     // todo DIV
     const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo');
+    todoDiv.classList.add('todo'); // <div class="todo"></div>
+    // todoDiv.classList.add('todo2'); -> 클래스를 여러 개 가질 수 있음.
 
     // todo LI 
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo_item');
-    todoDiv.appendChild(newTodo);
+    newTodo.classList.add('todo_item'); // <li class="todo_item"</li>
+    todoDiv.appendChild(newTodo); // div 여는 tag와 닫는 tag 사이에 li가 들어감.
     if (todoInput.value === "") {
         return null
     }
@@ -48,11 +48,11 @@ function addTodo(event) {
 
 // DELETE & CHECK
 function deleteCheck(e) {
-    const item = e.target;
+    const item = e.target; // 이벤트를 실행한 주체
 
     // DELETE ITEM
     if (item.classList[0] === "delete_btn") {
-        const todo = item.parentElement;
+        const todo = item.parentElement; // item이 <botton>이고, 이것의 부모인 <div>가 todo.
 
         // ANIMATION TRANSITION
         todo.classList.add("fall")
